d76d7a2a3c982b01983975acd824034bd6869155
Connor Clark <cjamcl@google.com>
2019-11-06 15:14:25 -0800

    forgot required artifacts

--- lighthouse-core/audits/metrics/first-cpu-idle.js
+++ lighthouse-core/audits/metrics/first-cpu-idle.js
@@ -27,7 +27,7 @@ class FirstCPUIdle extends Audit {
       title: str_(i18n.UIStrings.firstCPUIdleMetric),
       description: str_(UIStrings.description),
       scoreDisplayMode: Audit.SCORING_MODES.NUMERIC,
-      requiredArtifacts: ['traces', 'devtoolsLogs'],
+      requiredArtifacts: ['traces', 'devtoolsLogs', 'TestedAsMobileDevice'],
     };
   }
 
